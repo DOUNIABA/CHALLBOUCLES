c4097a58ac8e1735eabcb88c9c3f76c63fb20673
DOUNIA BAHASSANE <dounia0bahassane@gmail.com>
2021-11-17 08:50:42 +0100

    ajout

---
+++ challengeboucles1.c
@@ -0,0 +1,25 @@
+Algorithme multiplication_nombre
+
+VARIABLES
+
+  nb EST_DU_TYPE NOMBRE
+
+  i EST_DU_TYPE NOMBRE
+
+DEBUT_ALGORITHME
+
+  AFFICHER "entrer un nombre"
+
+  LIRE nb
+
+  POUR i ALLANT_DE 1 A 10 
+
+  DEBUT_POUR
+
+  AFFICHERCALCUL  nb * i 
+
+  AFFICHER "\n"
+
+    FIN_POUR
+
+FIN_ALGORITHME
